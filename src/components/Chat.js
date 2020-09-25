ac126f34cec9153dd1396bcdb44efc1ceb20f6fa
vivek666 <amalhan43@gmail.com>
2020-09-26 02:11:51 +0530

    Layout of Chat footer implemented

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,14 +3,22 @@ import "../assets/Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import AttachFileOutlinedIcon from "@material-ui/icons/AttachFileOutlined";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import InsertEmoticonOutlinedIcon from "@material-ui/icons/InsertEmoticonOutlined";
+import MicIcon from "@material-ui/icons/Mic";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 
 function Chat() {
   const [seed, setSeed] = useState("");
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log("Typing detected", input);
+  };
 
   return (
     <div className="chat">
@@ -39,7 +47,21 @@ function Chat() {
           <span className="chat__timestamp">3:52am</span>
         </p>
       </div>
-      <div className="chat__footer"></div>
+      <div className="chat__footer">
+        <InsertEmoticonOutlinedIcon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Type a message"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a Message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
