08444a65283f6dd4067a9da903aa74d99afaa9b5
vivek666 <amalhan43@gmail.com>
2020-09-27 02:15:47 +0530

    Routing the chats using the chat options

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -6,14 +6,24 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import InsertEmoticonOutlinedIcon from "@material-ui/icons/InsertEmoticonOutlined";
 import MicIcon from "@material-ui/icons/Mic";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import { useParams } from "react-router";
+import db from "../firebase";
 
 function Chat() {
   const [seed, setSeed] = useState("");
   const [input, setInput] = useState("");
+  const { roomId } = useParams();
+  const [roomName, setRoomName] = useState("");
 
   useEffect(() => {
-    setSeed(Math.floor(Math.random() * 5000));
-  }, []);
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomName(snapshot.data().name));
+
+      setSeed(Math.floor(Math.random() * 5000));
+    }
+  }, [roomId]);
 
   const sendMessage = (e) => {
     e.preventDefault();
@@ -27,7 +37,7 @@ function Chat() {
       <div className="chat__header">
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerInfo">
-          <h3>Dumb Dumb</h3>
+          <h3>{roomName}</h3>
           <p>Last seen at ...</p>
         </div>
         <div className="chat__headerRight">
--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "../assets/SidebarChat.css";
 import { Avatar } from "@material-ui/core";
 import db from "../firebase";
+import { Link } from "react-router-dom";
 
 function SidebarChat({ addNewChat, name, id }) {
   const [seed, setSeed] = useState("");
@@ -20,13 +21,15 @@ function SidebarChat({ addNewChat, name, id }) {
   };
 
   return !addNewChat ? (
-    <div className="sidebarChat">
-      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-      <div className="sidebarChat__info">
-        <h2>{name}</h2>
-        <p>Last Message....</p>
+    <Link to={`/rooms/${id}`}>
+      <div className="sidebarChat">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="sidebarChat__info">
+          <h2>{name}</h2>
+          <p>Last Message....</p>
+        </div>
       </div>
-    </div>
+    </Link>
   ) : (
     <div className="sidebarChat" onClick={createChat}>
       <h2>Add new Chat</h2>
